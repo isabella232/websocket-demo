fe25fd9d20b3e01f2ca031d3e2b1a7d5971697f1
Mark Petrovic <mspetrovic@gmail.com>
2013-12-06 07:10:44 -0800

    initial commit

---
+++ src/main/java/com/xoom/research/App.java
@@ -0,0 +1,86 @@
+
+package com.xoom.research;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.xoom.oss.feathercon.FeatherCon;
+import com.xoom.oss.feathercon.ServletConfiguration;
+import com.xoom.oss.feathercon.WebSocketEndpointConfiguration;
+import org.eclipse.jetty.servlet.DefaultServlet;
+
+import javax.websocket.server.ServerEndpointConfig;
+import java.util.HashSet;
+import java.util.Set;
+
+public class App implements Consumer {
+
+    private Configurator serverEndpointConfigurator;
+    private Set<ServerSocket> sockets = new HashSet<ServerSocket>();
+
+    public static void main(String[] args) throws Exception {
+        new App().run();
+    }
+
+    private void run() throws Exception {
+        serverEndpointConfigurator = new Configurator();
+        ServerEndpointConfig config = ServerEndpointConfig.Builder
+                .create(ServerSocket.class, "/events")
+                .configurator(serverEndpointConfigurator)
+                .build();
+
+        WebSocketEndpointConfiguration wsconfig = new WebSocketEndpointConfiguration.Builder()
+                .withServerEndpointConfig(config)
+                .build();
+
+        ServletConfiguration staticContentConfig = new ServletConfiguration.Builder()
+                .withServletClass(DefaultServlet.class)
+                .withPathSpec("/*")
+                .withInitParameter("resourceBase", "html")
+                .build();
+
+        FeatherCon server = new FeatherCon.Builder()
+                .withPort(8080)
+                .withWebSocketConfiguration(wsconfig)
+                .withServletConfiguration(staticContentConfig)
+                .build();
+
+        GraphiteProducer producer = new GraphiteProducer();
+        producer.add(this);
+        producer.start();
+
+        server.start();
+        server.join();
+    }
+
+    @Override
+    public void consume(Object o) {
+        for (ServerSocket socket : sockets) {
+            socket.consume(o);
+        }
+    }
+
+    public class Configurator extends ServerEndpointConfig.Configurator {
+
+        private final Injector injector;
+
+        public Configurator() {
+            injector = Guice.createInjector(new WebSocketModule());
+        }
+
+        // The locus of Endpoint dependency injection with wired in collaborators.
+        @Override
+        public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
+            T instance = injector.getInstance(endpointClass);
+            sockets.add((ServerSocket) instance);
+            return instance;
+        }
+
+        private class WebSocketModule extends AbstractModule {
+            @Override
+            protected void configure() {
+                bind(ServerSocket.class).to(ServerSocket.class);  // Guice default behavior, but show something here.
+            }
+        }
+    }
+}
---
+++ src/main/java/com/xoom/research/ServerSocket.java
@@ -0,0 +1,48 @@
+package com.xoom.research;
+
+import javax.websocket.CloseReason;
+import javax.websocket.EncodeException;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ServerSocket implements Consumer {
+    private final Set<Session> sessions = new HashSet<Session>();
+
+    @OnOpen
+    public void onWebSocketConnect(Session session) throws IOException, EncodeException {
+        sessions.add(session);  // curious
+    }
+
+    @OnMessage
+    public void onWebSocketText(String message) throws IOException, EncodeException {
+        // this server is never sent messages
+    }
+
+    @OnClose
+    public void onWebSocketClose(CloseReason reason) {
+    }
+
+    @OnError
+    public void onWebSocketError(Throwable cause) {
+    }
+
+    @Override
+    public void consume(Object o) {
+        for (final Session session : sessions) {
+            try {
+                session.getBasicRemote().sendObject(o.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (EncodeException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
